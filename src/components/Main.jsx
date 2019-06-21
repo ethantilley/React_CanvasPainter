17d94d543a374b979d5302248b9ff2b19445af83
Ethan Tilley <1008464@student.sae.edu.au>
2019-06-21 15:37:41 +1000

    Further styling of the button and attemped saving of data

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,10 +1,12 @@
-   
+
 
 import reactCSS from 'reactcss'
 import { SketchPicker } from 'react-color'
 import React, { Component } from 'react';
 import '../App.css';
 import { SketchField, Tools } from 'react-sketch';
+import dataJson from './data.json.js';
+import dataJsonControlled from './data.json.controlled.js';
 
 
 class Main extends React.Component {
@@ -31,7 +33,7 @@ class Main extends React.Component {
         fillWithBackgroundColor: false,
         controlledSize: false,
         sketchWidth: 600,
-        sketchHeight: 600,
+        sketchHeight: 800,
         stretched: true,
         stretchedX: false,
         stretchedY: false,
@@ -39,7 +41,7 @@ class Main extends React.Component {
         originY: 'top'
     };
 
-    
+
     handleClick = () => {
 
         this.setState({ displayColorPicker: !this.state.displayColorPicker })
@@ -56,26 +58,22 @@ class Main extends React.Component {
         this.setState({
             color: color.rgb,
         })
-        
+
     };
 
     undoChange = () => {
 
+        this._sketch.undo();
 
-            this._sketch.undo();
-            
-            this.setState({
-                canUndo: this._sketch.canUndo(),
-                canRedo: this._sketch.canRedo()
-            })
-          
-
-
+        this.setState({
+            canUndo: this._sketch.canUndo(),
+            canRedo: this._sketch.canRedo()
+        })
     };
 
     redoChange = () => {
         this._sketch.redo();
-        
+
         this.setState({
             canUndo: this._sketch.canUndo(),
             canRedo: this._sketch.canRedo()
@@ -84,17 +82,53 @@ class Main extends React.Component {
 
     clearChanges = () => {
         this._sketch.clear();
-        
+
         this.setState({
+            controlledValue: null,
             canUndo: this._sketch.canUndo(),
             canRedo: this._sketch.canRedo()
         })
     };
+    componentDidMount = () => {
 
+        /*eslint-disable no-console*/
+
+        (function (console) {
+            console.save = function (data, filename) {
+                if (!data) {
+                    console.error('Console.save: No data');
+                    return;
+                }
+                if (!filename) filename = 'console.json';
+                if (typeof data === 'object') {
+                    data = JSON.stringify(data, undefined, 4)
+                }
+                var blob = new Blob([data], { type: 'text/json' }),
+                    e = document.createEvent('MouseEvents'),
+                    a = document.createElement('a');
+                a.download = filename;
+                a.href = window.URL.createObjectURL(blob);
+                a.dataset.downloadurl = ['text/json', a.download, a.href].join(':');
+                e.initMouseEvent('click', true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
+                a.dispatchEvent(e)
+            }
+        })(console);
+
+        /*eslint-enable no-console*/
+
+    };
     saveChanges = () => {
         let drawings = this.state.drawings;
         drawings.push(this._sketch.toDataURL());
-        this.setState({drawings: drawings});
+        this.setState({ drawings: drawings });
+
+    };
+
+    downloadChanges = () => {
+        this.setState({
+            controlledValue: dataJsonControlled
+        })
+
     };
 
     onSketchChange = () => {
@@ -102,7 +136,7 @@ class Main extends React.Component {
         let now = this._sketch.canUndo();
 
         if (prev !== now) {
-            this.setState({canUndo: now});
+            this.setState({ canUndo: now });
         }
     };
 
@@ -111,9 +145,9 @@ class Main extends React.Component {
         const styles = reactCSS({
             'default': {
                 color: {
-                  
+
                     background: `rgba(${this.state.color.r}, ${this.state.color.g}, ${this.state.color.b}, ${this.state.color.a})`,
-                
+
                 },
                 swatch: {
                     background: `rgba(${this.state.color.r}, ${this.state.color.g}, ${this.state.color.b}, ${this.state.color.a})`,
@@ -133,52 +167,70 @@ class Main extends React.Component {
                 },
             },
         });
-        let {controlledValue} = this.state;
+        let { controlledValue } = this.state;
 
         return (
-            <div>
+            <div className="App">
                 <div className="canvas-header">
-                    <button className="undo-button" style={styles.swatch} onClick={this.handleClick}>
-                        <div className="undo-button" style={styles.color} />
+                    <div className="control-buttons">
+                        <button className="color-picker" style={{ background: styles.color.background }} onClick={this.handleClick}>
+
+                        </button>
+                        {
+                            this.state.displayColorPicker ? <div style={styles.popover}>
+                                <div onClick={this.handleClose} />
+                                <SketchPicker color={this.state.color} onChange={this.handleChange} />
+                            </div> : null
+                        }
+
+                        <button
+                            className="basic-button"
+                            style={{ cursor: !this.state.canUndo ? 'not-allowed' : 'pointer' }}
+                            disabled={!this.state.canUndo} onClick={this.undoChange}
+                            title="Undo last change!"
+                        >
+                            Undo
+                        </button>
+                        <button
+                            className="basic-button"
+                            style={{ cursor: !this.state.canRedo ? 'not-allowed' : 'pointer' }}
+                            disabled={!this.state.canRedo}
+                            onClick={this.redoChange}
+                        >
+                            Redo
+                        </button>
+                        <button
+                            className="basic-button"
+                            style={{ cursor: !this.state.canUndo ? 'not-allowed' : 'pointer' }}
+                            disabled={!this.state.canUndo}
+                            onClick={this.clearChanges}
+                        >
+                            Clear
+                        </button>
+                        <button
+                            className="basic-button"
+                            onClick={this.saveChanges}
+                            style={{
+                                cursor: 'not-allowed',
+                                opacity: '0.6'
+                            }}
+                            disabled={true}
+                        >
+                            Save
+                        </button>
+                        <button
+                            className="basic-button"
+                            onClick={this.downloadChanges}
+                            disabled={true}
+                            style={{
+                                cursor: 'not-allowed',
+                                opacity: '0.6'
+                            }}
+                        >
+                            Load
                     </button>
-                    {
-                        this.state.displayColorPicker ? <div  className="undo-button" style={styles.popover}>
-                            <div  onClick={this.handleClose} />
-                            <SketchPicker color={this.state.color} onChange={this.handleChange} />
-                        </div> : null
-                    }
-                    <button 
-                        className="undo-button" 
-                        style= {{cursor: !this.state.canUndo ? 'not-allowed' : 'pointer'}} 
-                        disabled={!this.state.canUndo} onClick={this.undoChange} 
-                        title="Undo last change!"
-                        >                        
-                        Undo
-                        </button>
-                    <button 
-                        className="undo-button" 
-                        style= {{cursor: !this.state.canRedo ? 'not-allowed' : 'pointer'}} 
-                        disabled={!this.state.canRedo} 
-                        onClick={this.redoChange}
-                        >
-                        Redo
-                        </button>
-                    <button 
-                        className="undo-button" 
-                        style= {{cursor: !this.state.canUndo ? 'not-allowed' : 'pointer'}} 
-                        disabled={!this.state.canUndo} 
-                        onClick={this.clearChanges}
-                        >
-                        Clear
-                        </button>
-                    <button 
-                        className="undo-button" 
-                        onClick={this.saveChanges} 
-                        style= {{cursor: !this.state.canUndo ? 'not-allowed' : 'pointer'}} 
-                        disabled={!this.state.canUndo} 
-                        >
-                        Save
-                        </button>
+
+                    </div>
                 </div >
                 <SketchField
                     className="canvas"
@@ -190,6 +242,7 @@ class Main extends React.Component {
                     backgroundColor={this.state.fillWithBackgroundColor ? this.state.backgroundColor : 'transparent'}
                     width={this.state.controlledSize ? this.state.sketchWidth : null}
                     height={this.state.controlledSize ? this.state.sketchHeight : null}
+                    defaultValue={dataJson}
                     value={controlledValue}
                     forceValue={true}
                     onChange={this.onSketchChange}
--- src/index.js
+++ src/index.js
@@ -3,6 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+// const { Client } = require('pg')
+
+// const _client = new Client({
+//     user: "postgres",
+//     port: 5432,
+//     host: "ethanspc",
+//     database: "Canvas_Painter",
+// })
+
+// _client.connect()
+// .then(() => console.log("Connected Successfuly"))
+// .catch(e => console.error(e))
+// .finally(() => _client.end())
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
